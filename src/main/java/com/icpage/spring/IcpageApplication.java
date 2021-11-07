1131fb44eb67db5bbdb49c79fffc0f3a9a69108a
wjdwodnr5452 <wjdwodnr5452@naver.com>
2021-11-07 15:34:06 +0900

    첫번째 커밋!!

---
+++ src/main/java/com/icpage/spring/IcpageApplication.java
@@ -0,0 +1,13 @@
+package com.icpage.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcpageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/icpage/spring/IcpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icpage.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
